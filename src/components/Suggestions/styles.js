825787ef409190798d842ac8bf5d385a6a53d320
Caio Oliveira <caiolv@me.com>
2021-04-10 13:41:56 -0300

    fix: remover scroll indicator

--- src/components/Suggestions/styles.js
+++ src/components/Suggestions/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.ScrollView.attrs(() => ({
     horizontal: true,
+    showsHorizontalScrollIndicator: false,
     contentContainerStyle: {
         alignItems: 'center',
         paddingLeft: 16,
