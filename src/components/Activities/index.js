7a1f49e5ae709fca292458917768e45aea428f20
Caio Oliveira <caiolv@me.com>
2021-04-10 13:38:43 -0300

    add: atividades

---
+++ src/components/Activities/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Feather, MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+
+import {
+    Container,
+    Header,
+    Title,
+    Card,
+    CardHeader,
+    Avatar,
+    Description,
+    Bold,
+    CardBody,
+    UserName,
+    CardFooter,
+    Details,
+    Value,
+    Divider,
+    Date,
+    Actions,
+    Option,
+    OptionLabel
+} from './styles';
+
+import avatar from '../../images/avatar.png';
+
+export default function Activities() {
+  return (
+      <Container>
+          <Header>
+              <Title>Atividades</Title>
+          </Header>
+
+          <Card> 
+            <CardHeader>
+                <Avatar source={avatar} />
+                <Description>
+                    <Bold>Você</Bold> pagou a <Bold>@maateussilva</Bold>
+                </Description>
+            </CardHeader>
+            <CardBody>
+                <UserName>Caio Oliveira</UserName>
+            </CardBody>
+
+            <CardFooter>
+                <Details>
+                    <Value>R$ 18,00</Value>
+                    
+                    <Divider />
+
+                    <Feather name="lock" color="#fff" size={14} />
+                    <Date> Há 2 anos</Date>
+                </Details>
+
+                <Actions>
+                    <Option>
+                        <MaterialCommunityIcons name="comment-outline" size={14} color="#fff" />
+                        <OptionLabel>0</OptionLabel>
+                    </Option>
+
+                    <Option>
+                        <AntDesign name="hearto" size={14} color="#fff" />
+                        <OptionLabel>0</OptionLabel>
+                    </Option>
+                </Actions>
+            </CardFooter>
+          </Card>
+      </Container>
+  );
+}
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -12,6 +12,7 @@ import {
 } from './styles';
 
 import Suggestions from '../../components/Suggestions';
+import Activities from '../../components/Activities';
 
 export default function Home() {
     return (
@@ -29,6 +30,7 @@ export default function Home() {
                 </Header>
 
                 <Suggestions />
+                <Activities />
             </Container>
         </Wrapper>
     );
