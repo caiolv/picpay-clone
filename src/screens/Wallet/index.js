8097e38433132e91f17c480706b995f2ce28b9a9
Caio Oliveira <caiolv@me.com>
2021-04-12 11:35:04 -0300

    add: metodos de pagamento e codigo promo

--- src/screens/Wallet/index.js
+++ src/screens/Wallet/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Feather } from '@expo/vector-icons';
+import {
+    Feather,
+    MaterialCommunityIcons,
+    FontAwesome,
+    AntDesign
+} from '@expo/vector-icons';
+
+import { Switch } from 'react-native';
 
 import { 
     Wrapper,
@@ -10,8 +17,28 @@ import {
     Value,
     Bold,
     EyeButton,
-    Info
+    Info,
+    Actions,
+    Action,
+    ActionLabel,
+    UseBalance,
+    UseBalanceTitle,
+    PaymentMethods,
+    PaymentMethodsTitle,
+    Card,
+    CardBody,
+    CardDetails,
+    CardTitle,
+    CardDescription,
+    Img,
+    CardFooter,
+    AddCardButton,
+    AddCardLabel,
+    PromoCodeButton,
+    PromoCodeLabel,
  } from './styles';
+
+ import creditCard from '../../images/credit-card.png';
 
 const Wallet = () => {
   return (
@@ -33,8 +60,57 @@ const Wallet = () => {
                     <Info>
                         Seu saldo está rendendo 100% do CDI.
                     </Info>
+
+                    <Actions>
+                        <Action>
+                            <MaterialCommunityIcons name="cash" size={28} color="#fff" />
+                            <ActionLabel>Adicionar</ActionLabel>
+                        </Action>
+
+                        <Action>
+                            <FontAwesome name="bank" size={20} color="#fff" />
+                            <ActionLabel>Adicionar</ActionLabel>
+                        </Action>
+                    </Actions>
                 </HeaderContainer>
             </Header>
+
+            <UseBalance>
+                <UseBalanceTitle>Usar saldo ao pagar</UseBalanceTitle>
+
+                <Switch />
+            </UseBalance>
+            <PaymentMethods>
+                <PaymentMethodsTitle>
+                Forma de pagamento
+                </PaymentMethodsTitle>
+
+                <Card>
+                    <CardBody>
+                        <CardDetails>
+                            <CardTitle>Cadastre seu cartão de crédito</CardTitle>
+
+                            <CardDescription>
+                                Cadastre um cartão de crédito para poder fazer pagamentos mesmo quando não tiver saldo no seu PicPay.
+                            </CardDescription>
+                        </CardDetails>
+
+                        <Img source={creditCard} resizeMode="contain" />
+                    </CardBody>
+
+                    <AddCardButton>
+                        <AntDesign name="pluscircleo" size={25} color="#0DB060" />
+                        <AddCardLabel>Adicionar cartão de crédito</AddCardLabel>
+                    </AddCardButton>
+
+                </Card>
+
+                <PromoCodeButton>
+                    <MaterialCommunityIcons name="ticket-percent-outline" size={20} color="#0DB060" />
+
+                    <PromoCodeLabel>Usar código promocional</PromoCodeLabel>
+                </PromoCodeButton>
+            </PaymentMethods>
         </Wrapper>
     );
 }
--- src/screens/Wallet/styles.js
+++ src/screens/Wallet/styles.js
@@ -46,3 +46,108 @@ export const Info = styled.Text`
     font-size: 14px;
     font-weight: bold;
 `;
+
+export const Actions = styled.View`
+    flex-direction: row;
+    margin-top: 40px;
+`;
+
+export const Action = styled.TouchableOpacity`
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid rgba(255, 255, 255, 0.6);
+    width: 140px;
+    height: 45px;
+    border-radius: 25px;
+    margin: 0px 10px;
+`;
+
+export const ActionLabel = styled.Text`
+    color: #fff;
+    font-size: 16px;
+    margin-left: 10px;
+`;
+
+export const UseBalance = styled.View`
+    background: #1c1c1e;
+    height: 60px;
+    flex-direction: row;
+    padding: 0 16px;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const UseBalanceTitle = styled.Text`
+    color: #fff;
+    font-size: 16px;
+    font-weight: 500;
+`;
+
+export const PaymentMethods = styled.View`
+    margin-top: 25px;
+    padding: 0  16px;
+`;
+export const PaymentMethodsTitle = styled.Text`
+    color: #8E8E93;
+    text-transform: uppercase;
+`;
+
+export const Card = styled.View`
+    background: #1E232A;
+    padding: 20px;
+    border-radius: 8px;
+    margin-top: 10px;
+`;
+
+export const CardBody = styled.View`
+    flex-direction: row;
+`;
+
+export const CardDetails = styled.View`
+    flex: 1;
+    margin-right: 20px;
+`;
+
+export const CardTitle = styled.Text`
+    color: #fff;
+    font-size: 18px;
+    font-weight: bold;
+`;
+
+export const CardDescription = styled.Text`
+    font-size: 14px;
+    color: rgba(255, 255, 255, 0.8);
+    margin-top: 15px;
+`;
+
+export const Img = styled.Image`
+    width: 60px;
+`;
+
+export const AddCardButton = styled.TouchableOpacity`
+    flex-direction: row;
+    align-items: center;
+    margin-top: 20px;
+`;
+
+export const AddCardLabel = styled.Text`
+    color: #0DB060;
+    margin-left: 10px;
+    font-size: 17px;
+`;
+
+export const PromoCodeButton = styled.TouchableOpacity`
+    flex-direction: row;
+    align-items: center;
+    margin-top: 25px;
+    align-self: center;
+`;
+
+export const PromoCodeLabel = styled.Text`
+    color: #0DB060;
+    font-size: 16px;
+    font-weight: bold;
+    margin-left: 15px;
+    text-decoration-line: underline;
+`;
